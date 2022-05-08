c58fea89a7d85f86211c20a7efd7e72afd985f3f
Riccardo Roccaro <r0cc4rd0@gmail.com>
2022-05-08 10:19:11 +0200

    Add longhorn snapshot and backup support

--- code/main.py
+++ code/main.py
@@ -1,5 +1,5 @@
 import os
-
+from datetime import datetime
 from time import sleep
 
 from kubernetes import config
@@ -8,8 +8,11 @@ from kubernetes.client.api import core_v1_api
 from kubernetes.client.rest import ApiException
 from kubernetes.stream import stream
 
+import longhorn
+
 # Declaring global variables
 k8s_api_instance = None
+lh_client = None
 
 # Declaring global const
 ENTER_MAINTENANCE_CMD='runuser -u www-data -- php occ maintenance:mode --on'
@@ -28,6 +31,11 @@ def init_kubernetes_api():
     # Create core api object
     global k8s_api_instance
     k8s_api_instance = core_v1_api.CoreV1Api()
+
+
+def init_longhorn_client(longhorn_url):
+    global lh_client
+    lh_client = longhorn.Client(url=longhorn_url)
 
 
 def get_pod_by_label(namespace, app_name):
@@ -78,26 +86,51 @@ def enter_maintenance_mode_nextcloud(namespace, app_name):
         exit_maintenance_mode_nextcloud(namespace=namespace,app_name=app_name)
         exit(4)
 
+
 def exit_maintenance_mode_nextcloud(namespace, app_name):
     resp = exec_container_command(namespace=namespace,app_name=app_name, command=EXIT_MAINTENANCE_CMD)
     if resp != "Maintenance mode disabled\n":
         print("Error exiting maintenance mode. You have to do it by hands")
         exit(5)
-        
-def create_mysql_backup():
-    pass
 
-def create_snapshots():
-    pass
 
-def create_backups():
+def get_pv_name_by_pvc_name(namespace,pvc_name):
+    global k8s_api_instance
+    pvc=k8s_api_instance.read_namespaced_persistent_volume_claim(namespace=namespace, name=pvc_name)
+    return pvc.spec.volume_name
+
+#TODO Add backup file path
+def create_mysql_backup(namespace,db_name,password):
+    command = "mysqldump --add-drop-database --add-drop-table --lock-all-tables --result-file=backup.sql --password="+password+" --all-databases"
+    exec_container_command(namespace=namespace,app_name=db_name,command=command)
+
+
+def create_volume_backup_or_snapshot(backup_type, backup_name_suffix, volume_name, namespace, app_name):
+    # Retrieve the volume to backup/snapshot
+    global client
+    volume = client.by_id_volume(id=get_pv_name_by_pvc_name(namespace=namespace,pvc_name=volume_name))
+
+    # Create the snapshot
+    snapshot=volume.snapshotCreate(name=volume_name+"__"+backup_name_suffix)
+
+    # Create backup from snapshot if required
+    if backup_type == 'FULL-BACKUP':
+        # Create the backup
+        volume.snapshotBackup(name=snapshot.name)
+    else:
+        print("Unexpected error.")
+        exit_maintenance_mode_nextcloud(namespace=namespace,app_name=app_name)
+        exit(7)
+
+
+def clean_old_backup_snapshot():
     pass
 
 def main():
     # Read ancd check the environment variables
-    backup_type=os.getenv('BACK_TYPE')
+    backup_type=os.getenv('BACKUP_TYPE')
     if backup_type not in ['FULL-BACKUP', 'SNAPSHOT']:
-        print('Wrong backup type. BACK_TYPE is mandatory and must be either "FULL_BACKUP" or "SNAPSHOT"')
+        print('Wrong backup type. BACKUP_TYPE is mandatory and must be either "FULL_BACKUP" or "SNAPSHOT"')
         exit(1)
 
     namespace=os.getenv('NAMESPACE')
@@ -108,18 +141,55 @@ def main():
     if app_name == None:
         print('"APP_NAME" environment variable is mandatory')
         exit(2)
+    
+    db_name=os.getenv('DB_APP_NAME')
+    if db_name == None:
+        print('"DB_APP_NAME" environment variable is mandatory')
+        exit(8)
+
+    db_password=os.getenv('DB_ROOT_PASSWORD')
+    if db_password == None:
+        print('"DB_ROOT_PASSWORD" environment variable is mandatory')
+        exit(8)
 
     longhorn_url=os.getenv('LONGHORN_URL')
     if longhorn_url == None:
         print('The LONGHORN_URL env variable is mandatory.')
         exit(6)
 
-    init_kubernetes_api()
+    mysql_backup_volume_name=os.getenv('MYSQL_BACKUP_VOLUME_NAME')
+    if mysql_backup_volume_name == None:
+        print('The MYSQL_VOLUME_NAME env variable is mandatory.')
+        exit(6)
 
+    nextcloud_volume_name=os.getenv('NEXTCLOUD_VOLUME_NAME')
+    if nextcloud_volume_name == None:
+        print('The NEXTCLOUD_VOLUME_NAME env variable is mandatory.')
+        exit(6)
+
+    # Init kubernetes and longhorn clients
+    init_kubernetes_api()
+    init_longhorn_client(longhorn_url=longhorn_url)
+
+    # Start backup process
     enter_maintenance_mode_nextcloud(namespace=namespace,app_name=app_name)
 
+    # Create DB backup
+    create_mysql_backup(namespace=namespace,db_name=db_name,password=db_password)
+
+    # Create volumes backup suffix 
+    backup_name_suffix=datetime.now().strftime("%d-%m-%Y__%H-%M-%S")
+
+    # Create longhorn snapshots and backup
+    create_volume_backup_or_snapshot(backup_type=backup_type, backup_name_suffix=backup_name_suffix, volume_name=mysql_backup_volume_name, namespace=namespace)
+    create_volume_backup_or_snapshot(backup_type=backup_type, backup_name_suffix=backup_name_suffix, volume_name=nextcloud_volume_name, namespace=namespace)
+
+    # Complete backup process
     exit_maintenance_mode_nextcloud(namespace=namespace,app_name=app_name)
-    
+
+    # Clean old backup/snapshot
+    clean_old_backup_snapshot()
+
     return 0
 
 
