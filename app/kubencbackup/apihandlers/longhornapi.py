1e394c82b5f41211739180bf82cf9ea876e7f919
Riccardo Roccaro <r0cc4rd0@gmail.com>
2022-05-22 23:01:28 +0200

    Fix longhornapi excep handling + code enhancement

--- app/kubencbackup/apihandlers/longhornapi.py
+++ app/kubencbackup/apihandlers/longhornapi.py
@@ -1,6 +1,10 @@
+import functools
+
 from dateutil.parser import parse as dateutil_parse
+from dateutil.parser._parser import ParserError
 
 import kubencbackup.extlib.longhornlib as longhornlib
+from kubencbackup.extlib.longhornlib import ApiError, ClientApiError
 
 from kubencbackup.common.backupconfig import BackupConfig
 from kubencbackup.common.backupexceptions import ApiInstancesConfigException, ApiInstancesHandlerException
@@ -27,6 +31,7 @@ class LonghornApiInstanceConfig:
     @longhorn_url.setter
     def longhorn_url(self,longhorn_url):
         if longhorn_url == None:
+            self.log_err("longhorn_url is mandatory")
             raise LonghornApiInstanceConfigException(message='longhorn_url is mandatory')
         self.__longhorn_url=longhorn_url
     ### END ###
@@ -43,8 +48,12 @@ class LonghornApiInstanceConfig:
         else:
             try:
                 self.__nr_snapshots_to_retain = int(nr_snapshots_to_retain)
-            except (ValueError,TypeError) as e:
+            except (ValueError,TypeError):
+                self.log_err("nr_snapshot_to_retain must be a integer number")
                 raise LonghornApiInstanceConfigException(message="nr_snapshot_to_retain must be a integer number")
+            except:
+                self.log_err("Unknown error while setting nr_snapshot_to_retain")
+                raise LonghornApiInstanceConfigException(message="Unknown error while setting nr_snapshot_to_retain")
     ### END ###
 
     ### nr_backups_to_retain getter and setter ###
@@ -59,11 +68,27 @@ class LonghornApiInstanceConfig:
         else:
             try:
                 self.__nr_backups_to_retain = int(nr_backups_to_retain)
-            except (ValueError,TypeError) as e:
+            except (ValueError,TypeError):
+                self.log_err("nr_backups_to_retain must be a integer number")
                 raise LonghornApiInstanceConfigException(message="nr_backups_to_retain must be a integer number")
+            except:
+                self.log_err("Unknown error while setting nr_backups_to_retain")
+                raise LonghornApiInstanceConfigException(message="Unknown error while setting nr_backups_to_retain")
     ### END ###
 
 ### END - Config ###
+
+### Methods decorator ###
+def exec_only_if_conn_init(method):
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        if self.is_connection_initialized:
+            method(self, *args, **kwargs)
+        else:
+            self.log_err("Connection to Longhorn API not initialized")
+            raise LonghornApiInstanceHandlerException(message="Connection to Longhorn API not initialized")
+    return wrapper
+### END ###
 
 ### Handler ###
 class LonghornApiInstanceHandlerException(ApiInstancesHandlerException):
@@ -73,6 +98,8 @@ class LonghornApiInstanceHandlerException(ApiInstancesHandlerException):
 class LonghornApiInstanceHandler(Loggable):
     def __init__(self, config):
         super().__init__(name="LONGHORN-API##", log_level=2)
+
+        self.__is_connection_initialized = False
 
         if config == None or type(config) != LonghornApiInstanceConfig:
             self.log_err(err="the configuration object mustn't be None and must be a LonghornApiInstanceConfig instance")
@@ -86,10 +113,16 @@ class LonghornApiInstanceHandler(Loggable):
             self.client = longhornlib.Client(url=self.config.longhorn_url)
 
             self.log_info(msg="DONE. Longhorn API successfully initialized.")
-        except BaseException as e:
-            self.log_err(err="Unable to create the Longhorn API client instance.")
+        except (ApiError,ClientApiError):
+            self.log_err(err="Longhorn API error. Unable to create the Longhorn API client instance.")
             self.free_resources()
-            raise LonghornApiInstanceHandlerException(message="Error creating Longhorn API client instance. The error message is:\n" + e)
+            raise LonghornApiInstanceHandlerException(message="Longhorn API error. Error creating Longhorn API client instance")
+        except:
+            self.log_err(err="Unknown error. Unable to create the Longhorn API client instance")
+            self.free_resources()
+            raise LonghornApiInstanceHandlerException(message="Unknown error. Unable to create the Longhorn API client instance")
+
+        self.__is_connection_initialized = True
 
         return self
 
@@ -104,6 +137,7 @@ class LonghornApiInstanceHandler(Loggable):
         try:
             del(self.client)
             self.log_info(msg="Longhorn API resources successfully cleaned up")
+            self.__is_connection_initialized = False
         except (AttributeError,NameError):
             pass
         except:
@@ -138,7 +172,14 @@ class LonghornApiInstanceHandler(Loggable):
         self.__config=config
     ### END ###
 
+    ### is_connection_initialized getter###
+    @property
+    def is_connection_initialized(self):
+        return self.__is_connection_initialized
+    ### END ###
+
     ### Methods implementation ###
+    @is_connection_initialized
     def create_volume_snapshot(self, snapshot_name, pv_name):
         self.log_info(msg="Creating the "+ pv_name + " volume snapshot...")
         try:
@@ -154,10 +195,16 @@ class LonghornApiInstanceHandler(Loggable):
             volume.snapshotCreate(name=snapshot_name)
             self.log_info(msg="DONE. Snapshot named " + snapshot_name + " for " + pv_name + " volume successfully created")
 
-        except BaseException as e:
-            self.log_err(err="Unable to create the snapshot for the volume " + pv_name)
-            raise LonghornApiInstanceHandlerException(message="Unable to create the snapshot for the volume " + pv_name +". The error message is:\n" + e)
+        except LonghornApiInstanceHandlerException as e:
+            raise e
+        except (ApiError,ClientApiError):
+            self.log_err(err="Longhorn API error. Unable to create the snapshot for the volume " + pv_name)
+            raise LonghornApiInstanceHandlerException(message="Longhorn API error. Unable to create the snapshot for the volume " + pv_name)
+        except:
+            self.log_err(err="Unknown error. Unable to create the snapshot for the volume " + pv_name)
+            raise LonghornApiInstanceHandlerException(message="Unknown error. Unable to create the snapshot for the volume " + pv_name)
 
+    @is_connection_initialized
     def create_volume_backup(self, snapshot_name, pv_name):
         self.log_info(msg="Creating the "+ pv_name + " volume backup from snapshot " + snapshot_name + "...")
         try:
@@ -176,16 +223,21 @@ class LonghornApiInstanceHandler(Loggable):
                     break
             else:
                 self.log_err(err="Unable to find the snapshot named " + snapshot_name +". The backup cannot be done")
-                raise LonghornApiInstanceHandlerException(message="Unable to find the snapshot named " + snapshot_name +". The backup cannot be done.")
+                raise LonghornApiInstanceHandlerException(message="Unable to find the snapshot named " + snapshot_name +". The backup cannot be done")
 
             # Create the backup
             self.log_info(msg="Creating the backup...")
             volume.snapshotBackup(name=snapshot_name)
             self.log_info(msg="DONE. Backup from snapshot " + snapshot_name + " for " + pv_name + " volume successfully created")
-        except BaseException as e:
-            self.log_err(err="Unable to create the backup for the volume " + pv_name)
-            raise LonghornApiInstanceHandlerException(message="Unable to create the backup for the volume " + pv_name +". The error message is:\n" + e)
-    
+        except LonghornApiInstanceHandlerException as e:
+            raise e
+        except (ApiError,ClientApiError):
+            self.log_err(err="Longhorn API error. Unable to create the backup for the volume " + pv_name)
+            raise LonghornApiInstanceHandlerException(message="Longhorn API error. Unable to create the backup for the volume " + pv_name)
+        except:
+            self.log_err(err="Unknown error. Unable to create the backup for the volume " + pv_name)
+            raise LonghornApiInstanceHandlerException(message="Unknown error. Unable to create the backup for the volume " + pv_name)
+
 
     def create_volume_snapshot_and_backup(self, backup_name, pv_name):
         # Create the snapshot
@@ -193,7 +245,7 @@ class LonghornApiInstanceHandler(Loggable):
         # Create the backup
         self.create_volume_backup(snapshot_name=backup_name, pv_name=pv_name)
 
-
+    @is_connection_initialized
     def delete_snapshots_over_retain_count(self, pv_name):
         self.log_info("Deleting the snapshots older than the last " + str(self.config.nr_snapshots_to_retain) + "...")
         try:
@@ -210,14 +262,21 @@ class LonghornApiInstanceHandler(Loggable):
                 snaps=vol.snapshotList().data
             except:
                 self.log_err(err="Cannot find the snapshot list for the volume " + pv_name + ". Unable to continue")
-                raise LonghornApiInstanceHandlerException(message="Cannot find the snapshot list.")
+                raise LonghornApiInstanceHandlerException(message="Cannot find the snapshot list for the volume " + pv_name + ". Unable to continue")
 
             self.log_info(msg="DONE. Retrieving the snapshots to remove...")
             if len(snaps) > self.config.nr_snapshots_to_retain:
                 # Polulate a dictionary with the snapshot creation time as a key
-                snaps_dict={}
-                for snap in snaps:
-                    snaps_dict[dateutil_parse(snap.created)]=snap.name
+                try:
+                    snaps_dict={}
+                    for snap in snaps:
+                        snaps_dict[dateutil_parse(snap.created)]=snap.name
+                except ParserError:
+                    self.log_err("Snapshot creation date and time parsing error")
+                    raise LonghornApiInstanceHandlerException(message="Snapshot creation date and time parsing error")
+                except:
+                    self.log_err("Unknown error while parsing snapshot creation date and time")
+                    raise LonghornApiInstanceHandlerException(message="Unknown error while parsing snapshot creation date and time")
                 
                 # Retrieve the dictionary keys and sort them chronologically
                 keys=[*snaps_dict]
@@ -228,11 +287,16 @@ class LonghornApiInstanceHandler(Loggable):
                 while len(keys) > self.config.nr_snapshots_to_retain:
                     vol.snapshotDelete(name=snaps_dict[keys.pop(0)])
                 self.log_info(msg="DONE. successfully removed the older snapshots for the volume " + pv_name)
-        except BaseException as e:
-            self.log_err(err="Cannot delete snapshots over retain count")
-            raise LonghornApiInstanceHandlerException(message="Cannot delete snapshots over retain count due to the following issue:\n" +e)
+        except LonghornApiInstanceHandlerException as e:
+            raise e
+        except (ApiError,ClientApiError):
+            self.log_err(err="Longhorn API error. Cannot delete snapshots over retain count")
+            raise LonghornApiInstanceHandlerException(message="Longhorn API error. Cannot delete snapshots over retain count")
+        except:
+            self.log_err(err="Unknown error. Cannot delete snapshots over retain count")
+            raise LonghornApiInstanceHandlerException(message="Unknown error. Cannot delete snapshots over retain count")
 
-
+    @is_connection_initialized
     def delete_backups_over_retain_count(self, pv_name):
         self.log_info("Deleting the backups older than the last " + str(self.config.nr_backups_to_retain) + "...")
         try:
@@ -249,16 +313,23 @@ class LonghornApiInstanceHandler(Loggable):
                 backs=vol.backupList().data
             except:
                 self.log_err(err="Cannot find the backups list for the volume " + pv_name + ". Unable to continue")
-                raise LonghornApiInstanceHandlerException(message="Cannot find the backups list.")
+                raise LonghornApiInstanceHandlerException(message="Cannot find the backups list")
 
             self.log_info(msg="DONE. Retrieving the backups to remove...")
             if len(backs) > self.config.nr_backups_to_retain:
                 # Polulate a dictionary with the snapshot (linked to the backup) creation time as a key
-                backs_dict={}
-                for back in backs:
-                    # Check if the backup is already completed, otherwise it will be skipped
-                    if back.progress == 100:
-                        backs_dict[dateutil_parse(back.snapshotCreated)]=back.name
+                try:
+                    backs_dict={}
+                    for back in backs:
+                        # Check if the backup is already completed, otherwise it will be skipped
+                        if back.progress == 100:
+                            backs_dict[dateutil_parse(back.snapshotCreated)]=back.name
+                except ParserError:
+                    self.log_err("Snapshot creation date and time parsing error")
+                    raise LonghornApiInstanceHandlerException(message="Snapshot creation date and time parsing error")
+                except:
+                    self.log_err("Unknown error while parsing snapshot creation date and time")
+                    raise LonghornApiInstanceHandlerException(message="Unknown error while parsing snapshot creation date and time")
                 
                 # Retrieve the dictionary keys and sort them chronologically
                 keys=[*backs_dict]
@@ -269,9 +340,14 @@ class LonghornApiInstanceHandler(Loggable):
                 while len(keys) > self.config.nr_backups_to_retain:
                     vol.backupDelete(name=backs_dict[keys.pop(0)])
                 self.log_info(msg="DONE. successfully removed the older backups for the volume " + pv_name)
-        except BaseException as e:
-            self.log_err(err="Cannot delete backups over retain count")
-            raise LonghornApiInstanceHandlerException(message="Cannot delete backups over retain count due to the following issue:\n" +e)
+        except LonghornApiInstanceHandlerException as e:
+            raise e
+        except (ApiError,ClientApiError):
+            self.log_err(err="Longhorn API error. Cannot delete backups over retain count")
+            raise LonghornApiInstanceHandlerException(message="Longhorn API error. Cannot delete backups over retain count")
+        except:
+            self.log_err(err="Unknown error. Cannot delete backups over retain count")
+            raise LonghornApiInstanceHandlerException(message="Unknown error. Cannot delete backups over retain count")
 
     
     def delete_backups_and_snapshots_over_retain_count(self, pv_name):
