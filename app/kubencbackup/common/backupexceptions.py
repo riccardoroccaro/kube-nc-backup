8b15c2b4e48808b06b1ba882074085a3d829a3f6
Riccardo Roccaro <r0cc4rd0@gmail.com>
2022-05-22 19:48:32 +0200

    Fix exception handling in backupexceptions

--- app/kubencbackup/common/backupexceptions.py
+++ app/kubencbackup/common/backupexceptions.py
@@ -1,17 +1,6 @@
 class BackupException(Exception):
     def __init__(self,message):
-        self.message=message
-
-    def __str__(self) -> str:
-        return self.message
-
-    @property
-    def message(self):
-        return self.__message
-
-    @message.setter
-    def message(self,message):
-        self.__message=message
+        super().__init__(message)
 
 class ApiInstancesHandlerException(BackupException):
     def __init__(self,message):
